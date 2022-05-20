37252ebb16066fd498b3d2b411f1d7944085e8fc
claudioborja <claudio-borja@superiorapch.edu.ec>
2022-05-20 10:30:07 -0500

    cborja

--- curso_python_unidad_uno/clases/1.- operadores_aritmeticos.py
+++ curso_python_unidad_uno/clases/1.- operadores_aritmeticos.py
@@ -1,11 +1,5 @@
 '''
-Suma (+)
-Resta (-)
-Multiplicación (*)
-División (/)
-División entera (//)
-Módulo (%)
-Exponenciación (**)
+Suma (+), Resta (-), Multiplicación (*), División (/), División entera (//), Módulo (%), Exponenciación (**)
 '''
 print(5+5)
 print(5-5)
